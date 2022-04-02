cafd9b05670c109afd4485150e702ecfb0c83a86
Oleg Blotski <oblotskiyj@gmail.com>
2022-04-02 19:51:29 +0300

    change overflow for backlog and board

--- frontend/src/components/Board/BoardComponent/BoardComponent.jsx
+++ frontend/src/components/Board/BoardComponent/BoardComponent.jsx
@@ -9,23 +9,21 @@ const BoardComponent = ({isTaskInfo, columns, createColumnHandler}) => {
 
     return (
         <>
-            <div style={{position: "relative"}}>
-                <div className="board-container">
-                    <div className="project-path">
-                        <span className="project-text">Projects</span>
-                        <span> / </span>
-                        <span className="project-text">Project name</span>
-                    </div>
-                    <h2>Board Name</h2>
-                    <div className="search-tasks-container" style={{width: "320px"}}>
-                        <Search/>
-                    </div>
-                    <div className="columns-container">
-                        {columns.map((column) => <ColumnContainer key={column.id} column={column}/>)}
-                        <button onClick={createColumnHandler} className="button-add-column">
-                            <PlusSquareOutlined/>
-                        </button>
-                    </div>
+            <div className="board-container">
+                <div className="project-path">
+                    <span className="project-text">Projects</span>
+                    <span> / </span>
+                    <span className="project-text">Project name</span>
+                </div>
+                <h2>Board Name</h2>
+                <div className="search-tasks-container" style={{width: "320px"}}>
+                    <Search/>
+                </div>
+                <div className="columns-container">
+                    {columns.map((column) => <ColumnContainer key={column.id} column={column}/>)}
+                    <button onClick={createColumnHandler} className="button-add-column">
+                        <PlusSquareOutlined/>
+                    </button>
                 </div>
             </div>
             {isTaskInfo && <TaskInfoContainer/>}
